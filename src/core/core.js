27cb21b30e298cfb4f88e2e1d57c581503651aea
Valentin <valentin@elephantslab.com>
2021-12-16 22:25:45 +0200

    add post and get requests, axios, and baseURL

--- src/core/core.js
+++ src/core/core.js
@@ -1,12 +1,15 @@
+import axios from "axios";
 import {ethers} from "ethers";
 const contractAddress = "0x8Dc36fB73063Cfa9d18767522b2145A9ca9DF6F9";
 const NODE = "https://mainnet.aurora.dev";
 import abi from "./contractAbi.json";
+import config from '../assets/config.json';
 
 export default class Core {
     constructor(context) {
         this.context = context;
         this.init();
+        this.baseURL = config.baseURL;
     }
 
     async init() {
@@ -28,5 +31,41 @@ export default class Core {
         // console.log();
 
         return {tx: txReceipt, response: txResponse};
+    }
+    //**************************post requests**************************
+    async setNewLot(id,price){
+        const timestamp = Math.floor(new Date() / 1000);
+        await axios.post(`${this.baseURL}/setNewLot`, this.baseURL, {
+            id,
+            price,
+            date: timestamp
+        },{headers: { 'content-type': 'application/x-www-form-urlencoded' }});
+
+    }
+    async setNewBooking(nft_id, place_id, table_number){
+        await axios.post(`${this.baseURL}/setNewBooking`, {
+            nft_id,
+            place_id,
+            table_number
+        },{headers: { 'content-type': 'application/x-www-form-urlencoded' }});
+    }
+    //*********************************get requests **************************
+    async getUserActiveLots(userAddress){
+        const result = axios.get(`${this.baseURL}/getUserActiveLots`,{
+            baseURL: this.baseURL,
+            params:{
+                address: userAddress
+            }
+        })
+        return result.data
+    }
+    async getMetadata(nft_id){
+        const result = axios.get(`${this.baseURL}/metadata/:${nft_id}`,{
+            baseURL: this.baseURL,
+            params:{
+                id: nft_id
+            }
+        })
+        return result.data
     }
 }
