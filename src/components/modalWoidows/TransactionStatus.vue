f3a467c945bc939ffbe249f3aec9a83e8fa565bc
Valentin <valentin@elephantslab.com>
2021-12-18 00:33:22 +0200

    add status transaction modal, fixed profile page

--- src/components/modalWoidows/TransactionStatus.vue
+++ src/components/modalWoidows/TransactionStatus.vue
@@ -1,15 +1,35 @@
 <template>
-    <div>
-        <div v-if='showSuccess'>
-        Success 
+<transition name="modal">
+    <div class="modal">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div class="modal-header">
+            <slot name="header">
+                Transaction Status
+            </slot>
+            <button @click="emits='close'" class="btn-modal-close" v-on:click="updateIsOpenSellModal(false)">
+               <i class="i-close-line"></i>
+            </button>
+          </div>
+
+          <div class="modal-body">
+            <div>
+                <div v-if='true'>
+                Success
+                </div>
+                <div v-if='showPending'>
+                Pending 
+                </div>
+                <div v-if='showFail'>
+                Fail
+                </div>
+            </div>
+          </div>
         </div>
-        <div v-if='showPending'>
-        Pending 
-        </div>
-        <div v-if='showFail'>
-        Fail
-        </div>
+      </div>
     </div>
+  </transition>
+
 </template>
 <script>
 export default{
